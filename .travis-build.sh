0179bcce46a606d8be23ae5f197bfe1caa20bb26
Mike Post <mike@bitmancer.me>
2017-02-17 22:36:28 -0800

    Fix Travis build

--- .travis-build.sh
+++ .travis-build.sh
@@ -7,18 +7,52 @@ TRAVIS_OS_NAME=${TRAVIS_OS_NAME:-} # allow unset (which we'll handle below)
 
 ECHO=$(which echo) || (echo echo not found! && exit 1)
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 
+function cmakeInstallMac() {
     if [ "$(which cmake)" == "" ]; then
         brew update
         brew install cmake
     fi
+}
+
+
+function cmakeInstallLinux() {
+
+    # NOTE: Travis apt-get is serving up an old version of cmake, so let's just build it ourselves
+
+    buildDir="$HOME/cmake"
+
+    if [[ ! -d "$buildDir" || ! -d "$buildDir/bin" ]]; then
+
+        MKTEMP=$(which mktemp) || (echo mktemp not found! && exit 1)
+        RM=$(which rm) || (echo rm not found! && exit 1)
+        WGET=$(which wget) || (echo wget not found! && exit 1)
+        MV=$(which mv) || (echo mv not found! && exit 1)
+        TAR=$(which tar) || (echo tar not found! && exit 1)
+        MAKE=$(which make) || (echo make not found! && exit 1)
+
+        tempDir=$("$MKTEMP" -d)
+        function cleanup {
+            $RM -rf "$tempDir"
+        }
+        trap cleanup EXIT
+
+        "$WGET" -P "$tempDir" https://cmake.org/files/v3.7/cmake-3.7.2.tar.gz
+        (cd "$tempDir" && "$TAR" -xzf cmake-3.7.2.tar.gz)
+        (cd "$tempDir/cmake-3.7.2" && ./bootstrap --prefix="$buildDir" && "$MAKE" && "$MAKE" install)
+    else
+        "$ECHO" cmake appears to already exist at $buildDir
+    fi
+}
+
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+
+    cmakeInstallMac
 
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 
-    sudo apt-get -qq update
-    sudo apt-get install cmake
-
+    cmakeInstallLinux
     export CXX=g++-4.8
 
 else
@@ -26,9 +60,7 @@ else
 fi
 
 
-echo "***TEST***"
-
-env
+which cmake
 cmake --version
 
 npm run task ci
